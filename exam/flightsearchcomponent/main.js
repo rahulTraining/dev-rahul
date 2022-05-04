172453188c3028c5d4fef6305160a6575513ca76
Rahul <rahul.k@indianeagle.com>
2022-05-04 13:42:32 +0530

    searchcomponent

--- exam/flightsearchcomponent/main.js
+++ exam/flightsearchcomponent/main.js
@@ -4,12 +4,15 @@ var tripTypes = document.querySelector('.trip-types');
 var ways = document.querySelectorAll('.trip-types span');
 var displayWays = document.querySelector('#ways');
 
+var passengers = document.querySelector('.passengers')
+
 var classes = document.querySelector('.classes');
 var chevronClass = document.querySelector('.classes span');
 var classTypes = document.querySelector('.class-types');
 var classess = document.querySelectorAll('.class-types span');
 var displayClasses = document.querySelector('#classes');
 
+// trips
 
 trips.onclick = function() {
     tripTypes.classList.add('active');
@@ -29,6 +32,42 @@ ways.forEach((w) => {
     ways[0].click()
 })
 
+// swap button
+
+var swapButton = document.querySelector('.swap-btn i');
+var fromInput = document.querySelector('.from-input');
+
+var toInput = document.querySelector('.to-input');
+
+swapButton.onclick = function() {
+    this.classList.toggle('active');
+
+    if(fromInput.placeholder === "From?") {
+        fromInput.placeholder = "To?";
+    } else {
+        fromInput.placeholder = "From?"
+    }
+    if (toInput.placeholder === "To?") {
+        toInput.placeholder = "From?"
+    } else {
+        toInput.placeholder = "To?"
+    }
+
+    var fromValue = fromInput.value;
+    var toValue = toInput.value;
+
+    if(fromInput.value === fromValue || toInput.value === toValue) {
+        fromInput.value = toValue;
+        toInput.value = fromValue
+    }
+
+}
+// passengers
+
+passengers.onclick = function() {
+
+}
+
 // classes
 
 classes.onclick = function() {
@@ -36,14 +75,6 @@ classes.onclick = function() {
     chevronClass.classList.add('active');
     tripTypes.classList.remove('active');
     chevron.classList.remove('active');
-}
-window.onclick = function(e) {
-    if (e.target === document.documentElement) {
-        classTypes.classList.remove('active')
-        chevronClass.classList.remove('active')
-        tripTypes.classList.remove('active')
-        chevron.classList.remove('active')
-    }
 }
 classess.forEach((w) => {
     w.addEventListener('click',function() {
@@ -55,3 +86,13 @@ classess.forEach((w) => {
     })
     classess[0].click()
 })
+
+// window click function
+window.onclick = function(e) {
+    if (e.target === document.documentElement) {
+        classTypes.classList.remove('active')
+        chevronClass.classList.remove('active')
+        tripTypes.classList.remove('active')
+        chevron.classList.remove('active')
+    }
+}
