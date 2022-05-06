439f8980eb9e788881118afff99d19bfbeb8715b
Rahul <rahul.k@indianeagle.com>
2022-05-06 18:46:39 +0530

    focus

--- exam/flightsearchcomponent/main.js
+++ exam/flightsearchcomponent/main.js
@@ -20,10 +20,20 @@ var btns = document.querySelectorAll('.btns');
 var types = document.querySelectorAll('.types');
 
 var swapButton = document.querySelector('.swap-btn i');
+var focusInput = document.querySelectorAll('.focus-input')
 var fromInput = document.querySelector('.from-input');
 var toInput = document.querySelector('.to-input');
-var fromInput = document.querySelector('.from-input');
-var fromSearchField = document.querySelector('.from-search-field')
+var fromfieldInput = document.querySelectorAll('.search-field input');
+var searchField = document.querySelectorAll('.search-field');
+
+var searchfieldfromInput = document.querySelector('.from-div .search-field input');
+var searchfieldtoInput = document.querySelector('.to-div .search-field input');
+
+var signIn = document.querySelector('.from-div .search-field .fa-userr');
+var wheretoGo = document.querySelector('.to-div .search-field .bi-globee');
+var bixlg = document.querySelectorAll('.bi-x');
+
+var searchBtn = document.querySelector('.search-div')
 
 btns.forEach((b,index) => {
     b.addEventListener('click',function() {
@@ -40,9 +50,29 @@ ways.forEach((w) => {
         w.classList.add('activeBg');
         displayWays.innerText = w.innerText;
         tripTypes.classList.remove('active');
-        chevron.classList.remove('active')
+        chevron.classList.remove('active');
     })
     ways[0].click()
+})
+
+// input search focus
+
+focusInput.forEach((f,index) => {
+    f.addEventListener('focus',function() {
+        searchField.forEach(ff => ff.classList.remove('active'))
+        searchField[index].classList.add('active')
+    })
+})
+bixlg.forEach((x,index) => {
+    x.onclick = function() {
+        searchField.forEach(ff => ff.classList.remove('active'))
+        searchField[index].classList.remove('active')
+    }
+})
+fromfieldInput.forEach((i,index) => {
+    i.addEventListener('change',function() {
+        focusInput[index].value = this.value
+    })
 })
 
 // swap button
@@ -69,6 +99,16 @@ swapButton.onclick = function() {
         toInput.value = fromValue
     }
 
+    if (searchfieldfromInput.placeholder === "From?") {
+        searchfieldfromInput.placeholder = "To?"
+    } else {
+        searchfieldfromInput.placeholder = "From?"
+    }
+    if (searchfieldtoInput.placeholder === "To?") {
+        searchfieldtoInput.placeholder = "From?"
+    } else {
+        searchfieldtoInput.placeholder = "To?"
+    }
 }
 // passengers
 
@@ -91,13 +131,6 @@ classess.forEach((w) => {
 
 
 
-// input search focus
-
-fromInput.onfocus = function() {
-    fromSearchField.classList.add('active')
-}
-
-
 // window click function
 window.onclick = function(e) {
     if (e.target === document.documentElement) {
@@ -107,5 +140,6 @@ window.onclick = function(e) {
         chevron.classList.remove('active')
         passengerTypes.classList.remove('active')
         bagTypes.classList.remove('active')
+        searchField.forEach(ff => ff.classList.remove('active'))
     }
 }
